d6668b3976c2f7f24f962a62c2dd5f0cf04e3314
darre <darengt123@gmail.com>
2025-02-25 11:54:36 +0200

    Tervehdys Komponentti

---
+++ harjoitus0/src/App.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Hello from "./Hello.jsx";
+
+function App() {
+  return (
+    <div>
+      <Hello name="Ville" />
+    </div>
+  );
+}
+
+export default App;
---
+++ harjoitus0/src/Hello.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+function Hello(props) {
+  return <p>Hei {props.name}</p>;
+}
+
+export default Hello;
